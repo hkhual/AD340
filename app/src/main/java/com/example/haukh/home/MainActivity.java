94c2693c200c92bb8a1adbde22782a9257eba867
hkhual <24612635+hkhual@users.noreply.github.com>
2018-04-24 23:57:43 -0700

    Add files via upload

---
+++ app/src/main/java/com/example/haukh/home/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.haukh.home;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.haukh.home";
+
+    private RecyclerView scroll;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    //Called when  the user taps the Send button
+    public void sendMessage(View view){
+
+        //Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+
+        EditText editText = (EditText)findViewById(R.id.editText);
+
+        String message = editText.getText().toString();
+
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+         startActivity(intent);
+
+
+    }
+
+
+    //run this method when the user click a button
+    //This method will open another activity for movie informations
+    public void movieInfo(View view){
+
+           Intent intent = new Intent(this, Movies.class);
+
+           startActivity(intent);
+
+
+
+
+    }
+
+
+
+    public void ButtonClick(View view){
+
+        Button toastButton = this.findViewById(R.id.button3);
+
+        //---------------------------
+        //Display in short period of time
+        Toast.makeText(getApplicationContext(), "You clicked a button 2",
+                Toast.LENGTH_SHORT).show();
+
+        //--------------------------------
+        //Display in long period of time
+        //Toast.makeText(getApplicationContext(), "You clicked button3",
+               // Toast.LENGTH_LONG).show();
+    }
+
+
+
+    public void ButtonClick3(View view){
+
+        //Display in short period of time
+        //Toast
+        Toast.makeText(getApplicationContext(), "You clicked a button 3",
+                Toast.LENGTH_SHORT).show();
+
+
+
+    }
+
+
+    public void buttonClick4(View view){
+
+        //Display a toast in short period of time
+        Toast.makeText(getApplicationContext(), "You clicked a button 4",
+                Toast.LENGTH_SHORT).show();
+
+
+
+    }
+
+
+
+
+}
